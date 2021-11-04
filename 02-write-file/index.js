68e6e6a63ba5f819e000b1252da3919cfa334856
shadowinhaze <shadowinhaze@gmail.com>
2021-11-04 17:32:05 +0100

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const checkIt = (str) => {
+    if (str.trim() === 'exit') {
+        return rl.close()
+    }
+}
+
+const filePath = path.join(__dirname, 'input.txt');
+let writableStream = fs.createWriteStream(filePath, {flags: 'a+'});
+
+rl.question('Hi, write some text? \n', userInput => {
+    checkIt(userInput);
+    writableStream.write(userInput);
+    rl.on('line', userInput => {
+        checkIt(userInput);
+        writableStream.write(`\n${userInput}`);
+    })
+});
+
+rl.on('close', function() {
+    console.log('\nThank you, bye!!!');
+    process.exit(0);
+});

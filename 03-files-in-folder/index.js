2ffc6b2c0868bfb43f3599fa97ed508ab5b1bbcf
shadowinhaze <shadowinhaze@gmail.com>
2021-11-04 19:33:37 +0100

    refactor: add using nodejs modules for fileName and ﬁleExt at 03 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,8 +9,10 @@ const pathToFolder = path.join(__dirname, 'secret-folder');
         for (const file of files) {
             if (file.isFile()) {
                 const fileStats = await fs.stat(path.join(pathToFolder, file.name));
-                const fileName = file.name.replace(/^(.*)\.(.*)$/, '$1');
-                const fileExt = file.name.replace(/^(.*)\.(.*)$/, '$2');
+                // const fileName = file.name.replace(/^(.*)\.(.*)$/, '$1');
+                // const fileExt = file.name.replace(/^(.*)\.(.*)$/, '$2');
+                const fileName = await path.parse(path.join(pathToFolder, file.name)).name;
+                const fileExt = await path.parse(path.join(pathToFolder, file.name)).ext.slice(1);
                 const fileSize = await fileStats.size;
                 console.log(`${fileName} - ${fileExt} - ${fileSize / 1000}kb`)
             };

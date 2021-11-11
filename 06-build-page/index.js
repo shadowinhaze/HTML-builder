41af66b1d0f4be066ecadf35d392cac8af6b8236
shadowinhaze <shadowinhaze@gmail.com>
2021-11-11 15:11:18 +0100

    fix: fix 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,6 +9,9 @@ const pathToComponents = path.join(__dirname, 'components');
 const pathToStyles = path.join(__dirname, 'styles');
 const pathToDist = path.join(__dirname, 'project-dist');
 
+fsp.mkdir(pathToDist, { recursive: true });
+fsp.mkdir(path.join(pathToDist, 'assets'), { recursive: true });
+
 copyier(__dirname, 'assets', 'project-dist/assets');
 bundler(__dirname, 'styles', 'project-dist', 'style.css');
 
@@ -16,42 +19,35 @@ const fileResultIndexHtml = path.join(pathToDist, 'index.html');
 
 const reBirth = (filePath, replacer) => (replacer) ? fs.writeFile(filePath, replacer, err => (err) ? console.log(err) : true) : fs.writeFile(filePath, '', err => (err) ? console.log(err) : true);
 async function getContent(filePath) {
-    const fileSource = await fsp.open(filePath);
-    const fileData = await fileSource.readFile({ encoding: 'utf-8' });
-    fileSource.close();
-    return fileData;
+  const fileSource = await fsp.open(filePath);
+  const fileData = await fileSource.readFile({ encoding: 'utf-8' });
+  fileSource.close();
+  return fileData;
 }
 
 (async () => {
-    try {
-        reBirth(fileResultIndexHtml, await getContent(fileTemplateHtml));
-        const componentsChunks = await fsp.readdir(pathToComponents, { withFileTypes: true });
-        for (const componentsChunk of componentsChunks) {
-            if (componentsChunk.isFile()) {
-                const pathToChunk = path.join(pathToComponents, componentsChunk.name);
-                const chunkExt = await path.parse(pathToChunk).ext.slice(1);
-                const chunkName = await path.parse(pathToChunk).name;
-                if (chunkExt === 'html') {
-                    const chunkContent = await getContent(pathToChunk);
-                    const chunkPlace = new RegExp(`{{${chunkName}}}`, 'g');
-                    fs.readFile(fileResultIndexHtml, 'utf-8', (err, data) => {
-                        if (err) throw err;
-                        const replacer = data.replace(chunkPlace, chunkContent);
-                        fs.writeFile(fileResultIndexHtml, replacer, 'utf-8', err => {
-                            if (err) throw err;
-                        });
-                    })
-                }
-            };
-        };
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    reBirth(fileResultIndexHtml, await getContent(fileTemplateHtml));
+    const componentsChunks = await fsp.readdir(pathToComponents, { withFileTypes: true });
+    for (const componentsChunk of componentsChunks) {
+      if (componentsChunk.isFile()) {
+        const pathToChunk = path.join(pathToComponents, componentsChunk.name);
+        const chunkExt = await path.parse(pathToChunk).ext.slice(1);
+        const chunkName = await path.parse(pathToChunk).name;
+        if (chunkExt === 'html') {
+          const chunkContent = await getContent(pathToChunk);
+          const chunkPlace = new RegExp(`{{${chunkName}}}`, 'g');
+          let fileResultIndexHtmlContent = await getContent(fileResultIndexHtml);
+          const replacer = fileResultIndexHtmlContent.replace(chunkPlace, chunkContent);
+          fs.writeFile(fileResultIndexHtml, replacer, (err) => {
+            if (err) {
+              console.log(err)
+            }
+          })
+        }
+      };
+    };
+  } catch (err) {
+    console.error(err);
+  }
 })();
-
-
-
-
-
-
-
